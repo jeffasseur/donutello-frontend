6efe913ce675f37c35612e8015b9023dfa06e542
JEF .FASSEUR <91196774+jeffasseur@users.noreply.github.com>
2022-11-26 20:17:23 +0100

    feat: add html layout in Home view no styling

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,43 @@
 <template>
-    <div>
-        Home screen
+    <div class="home">
+        <header>
+            <h1>Donuts op maat voor jouw bedrijf</h1>
+            <p>
+                Maak hier jouw donuts in de
+                huisstijl van jouw bedrijf 
+                of organisatie in onze nieuwe 3D
+                configurator
+            </p>
+            <a href="#" class="btn btn--lemon">Maak je eigen <br> donut</a>
+        </header>
+        <section class="stappenplan">
+            <h2>Maak je donut in <span>6 stappen</span></h2>
+            <ol>
+                <li>Kies je <span>glazuur</span></li>
+                <li>Selecteer je <span>topping</span></li>
+                <li>Upload je  <span>logo</span></li>
+                <li>Vul je <span>email adres</span> in</li>
+                <li><span>Bestel</span> je donuts</li>
+                <li><span>Deel</span> jouw <span>3D donut</span></li>
+            </ol>
+            <a href="#" class="btn btn--strawberry">Start hier</a>
+        </section>
+        <section class="clients">
+            <h2>Tevreden klanten</h2>
+            <div class="clients__carousel">
+                <img src="#" alt="#" class="clients__carousel__img">
+                <img src="" alt="" class="clients__carousel__img">
+                <img src="" alt="" class="clients__carousel__img">
+                <img src="" alt="" class="clients__carousel__img">
+                <img src="" alt="" class="clients__carousel__img">
+                <img src="" alt="" class="clients__carousel__img">
+                <img src="" alt="" class="clients__carousel__img">
+                <img src="" alt="" class="clients__carousel__img">
+            </div>
+        </section>
     </div>
 </template>
 
 <style lang="scss">
-    div {
-        color: #FF00FF;
-    }
+@import './sass/app.scss';
 </style>
