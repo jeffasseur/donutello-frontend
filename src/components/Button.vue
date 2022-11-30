e67ec8b262f2554bd2abfea7900383a90ce0336f
JEF .FASSEUR <91196774+jeffasseur@users.noreply.github.com>
2022-11-30 11:24:19 +0100

    feat: add button component

---
+++ src/components/Button.vue
@@ -0,0 +1,10 @@
+<script setup>
+    const props = defineProps({
+    text: String,
+    class: String
+    })
+</script>
+
+<template>   
+    <button :class="props.class">{{ props.text }}</button>
+</template>
