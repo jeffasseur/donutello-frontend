3b3eaf1ab76eb5ec42f5b9af3fdf19733a86907d
JEF .FASSEUR <91196774+jeffasseur@users.noreply.github.com>
2022-12-12 17:13:29 +0100

    feat: rewrite router

--- src/router/index.js
+++ src/router/index.js
@@ -1,37 +1,34 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from './../views/Home.vue'
 import Login from './../views/Login.vue'
 import Dashboard from './../views/Dashboard.vue'
-import ChangePassword from '../views/ChangePassword.vue'
-import Donut from '../views/DonutDetail.vue'
-
-const routes = [
-  {
-    path: '/',
-    component: Home
-  },
-  {
-    path: '/login',
-    component: Login
-  },
-  {
-    path: '/dashboard',
-    name: 'Dashboard',
-    component: Dashboard
-  },
-  {
-    path: '/login/change-password',
-    component: ChangePassword
-  },
-  {
-    path: '/dashboard/donut/:id',
-    component: Donut
-  }
-]
+import ChangePassword from './../views/ChangePassword.vue'
+import Donut from './../views/DonutDetail.vue'
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
-})
+  routes: [
+    {
+      path: '/',
+      component: Home
+    },
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/dashboard',
+      component: Dashboard
+    },
+    {
+      path: '/login/change-password',
+      component: ChangePassword
+    },
+    {
+      path: '/dashboard/donut/:id',
+      component: Donut
+    }
+  ]
+});
 
 export default router
