b45221806f114916ce494a6760a41a62a274421b
Sienvdb <sienvdbergh@gmail.com>
2022-12-13 22:43:26 +0100

    feat: add lottie animation to loading screen

--- src/views/Loading.vue
+++ src/views/Loading.vue
@@ -1,14 +1,20 @@
 <script setup>
 // import navigation
 import Navigation from './../components/Navigation.vue'
-</script>
+import "@lottiefiles/lottie-player";</script>
 
 <template>
     <Navigation />
+    <body>
+        <!--insert lottie animation-->
 
+<lottie-player src="https://lottie.host/3cab8f75-ee6c-4e54-9b68-00e0bd5817bb/TpeJy1Q59f.json" background="transparent" speed="1" style="width: 300px; height: 300px;" loop autoplay></lottie-player>    </body>
 </template>
 
 <style lang="scss" scoped>
 @import './../sass/app.scss';
 
+body{
+    background-color: $heaven;
+}
 </style>
