ae7a5e443e8a66af17788ddc261abe9a7369c3dd
JEF .FASSEUR <91196774+jeffasseur@users.noreply.github.com>
2022-12-20 13:46:25 +0100

    style: fix width of nav bar of screen min-width of 1200px

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,153 +1,154 @@
 <script setup>
-    const openMenu = () => {
-        document.querySelector('.nav--mobile').style = 'top: 4.85rem';
-        // make cross from hamburger
-        document.querySelector('.icon--menu').style = 'display: none';
-        document.querySelector('.icon--close').style = 'display: block';
-    }
+const openMenu = () => {
+  document.querySelector(".nav--mobile").style = "top: 4.85rem";
+  // make cross from hamburger
+  document.querySelector(".icon--menu").style = "display: none";
+  document.querySelector(".icon--close").style = "display: block";
+};
 
-    const closeMenu = () => {
-        document.querySelector('.nav--mobile').style = 'top: -100vh';
-        // make hamburger from cross
-        document.querySelector('.icon--menu').style = 'display: block';
-        document.querySelector('.icon--close').style = 'display: none';
-    }
+const closeMenu = () => {
+  document.querySelector(".nav--mobile").style = "top: -100vh";
+  // make hamburger from cross
+  document.querySelector(".icon--menu").style = "display: block";
+  document.querySelector(".icon--close").style = "display: none";
+};
 </script>
 
 <template>
-    <nav>
-        <div class="nav__logo">
-            <a href="/">
-                <img src="/images/donuttello-logo.webp" alt="Logo Donuttello">
-            </a>
-        </div>
-        <div class="nav__menu">
-            <a href="/">Home</a>
-            <a href="https://the-donut-generator-rix11-h.vercel.app">Configurator</a>
-            <a href="/dashboard">Dashboard</a>
-        </div>
-        <a class="nav__hamburger">
-            <span class="material-icons-round icon--menu" @click="openMenu">menu</span>
-            <span class="material-icons-round icon--close" @click="closeMenu">close</span>
-        </a>
-        <a class="nav__login" href="/login">
-            <img src="/images/account.svg" alt="Account icon">
-        </a>
-    </nav>
-
-    <div class="nav--mobile">
-        <div class="nav__menu--mobile">
-            <a href="/">Home</a>
-            <a href="https://the-donut-generator-rix11-h.vercel.app">Configurator</a>
-            <a href="/dashboard">Dashboard</a>
-        </div>
+  <nav>
+    <div class="nav__logo">
+      <a href="/">
+        <img src="/images/donuttello-logo.webp" alt="Logo Donuttello" />
+      </a>
     </div>
+    <div class="nav__menu">
+      <a href="/">Home</a>
+      <a href="https://the-donut-generator-rix11-h.vercel.app">Configurator</a>
+      <a href="/dashboard">Dashboard</a>
+    </div>
+    <a class="nav__hamburger">
+      <span class="material-icons-round icon--menu" @click="openMenu">menu</span>
+      <span class="material-icons-round icon--close" @click="closeMenu">close</span>
+    </a>
+    <a class="nav__login" href="/login">
+      <img src="/images/account.svg" alt="Account icon" />
+    </a>
+  </nav>
+
+  <div class="nav--mobile">
+    <div class="nav__menu--mobile">
+      <a href="/">Home</a>
+      <a href="https://the-donut-generator-rix11-h.vercel.app">Configurator</a>
+      <a href="/dashboard">Dashboard</a>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-@import './../sass/app.scss';
+@import "./../sass/app.scss";
 
 /* MOBILE MENU */
 .nav--mobile {
-    background: $strawberry;
-    position: absolute;
-    top: -100vh;
-    left: 0;
-    width: calc(100% - 4rem);
-    padding: 2rem;
-    z-index: 5;
-    transition: all 0.5s ease-in-out;
+  background: $strawberry;
+  position: absolute;
+  top: -100vh;
+  left: 0;
+  width: calc(100% - 4rem);
+  padding: 2rem;
+  z-index: 5;
+  transition: all 0.5s ease-in-out;
 }
 .nav__menu--mobile {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    gap: 1.5em;
-    color: $white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1.5em;
+  color: $white;
 }
 .material-icons-round {
-    cursor: pointer;
-    color: $lemon;
-    transition: all .3s ease-in-out;
+  cursor: pointer;
+  color: $lemon;
+  transition: all 0.3s ease-in-out;
 }
 .icon--close {
-    display: none;
+  display: none;
 }
 /* END MOBILE MENU */
 
 nav {
-    background: $strawberry;
-    // border-bottom: 10px solid $white;
-    padding: .5rem 3rem;
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: calc(100vw - 6rem);
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-    z-index: 10;
+  background: $strawberry;
+  // border-bottom: 10px solid $white;
+  padding: 0.5rem 3rem;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: calc(100vw - 6rem);
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  z-index: 10;
 }
 .nav__logo {
-    position: absolute;
-    top: 2rem;
-    left: 3rem;
+  position: absolute;
+  top: 2rem;
+  left: 3rem;
 }
 .nav__menu {
-    display: none;
+  display: none;
 }
-.nav__menu a, .nav__menu--mobile a {
-    color: $white;
-    font-weight: 600;
-    text-transform: uppercase;
-    transition: all .3s ease-in-out;
+.nav__menu a,
+.nav__menu--mobile a {
+  color: $white;
+  font-weight: 600;
+  text-transform: uppercase;
+  transition: all 0.3s ease-in-out;
 }
 .nav__menu a:hover {
-    color: $lemon;
+  color: $lemon;
 }
 .nav__hamburger {
-    width: 1.5rem;
-    aspect-ratio: 2/1;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    gap: .25rem;
+  width: 1.5rem;
+  aspect-ratio: 2/1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 0.25rem;
 }
 .nav__hamburger .icon-bar {
-    height: 2px;
-    background: #FFF;
-    display: block;
+  height: 2px;
+  background: #fff;
+  display: block;
 }
 .nav__login {
-    display: inline-flex;
-    padding: 1.5rem;
+  display: inline-flex;
+  padding: 1.5rem;
 }
 
 @media screen and (min-width: 768px) {
-    .nav__menu {
-        display: flex;
-        gap: 2rem;
-    }
-    .nav__hamburger {
-        display: none;
-    }
+  .nav__menu {
+    display: flex;
+    gap: 2rem;
+  }
+  .nav__hamburger {
+    display: none;
+  }
 }
 
 @media screen and (min-width: 1200px) {
-    nav {
-        padding-left: 6rem;
-        padding-right: 6rem;
-        width: calc(100vw - 12rem);
-    }
-    .nav__logo {
-        left: 6rem;
-    }
-    .nav__menu {
-        display: flex;
-    }
-    .nav__hamburger {
-        display: none;
-    }
+  nav {
+    padding-left: 6rem;
+    padding-right: 6rem;
+    width: calc(100% - 12rem);
+  }
+  .nav__logo {
+    left: 6rem;
+  }
+  .nav__menu {
+    display: flex;
+  }
+  .nav__hamburger {
+    display: none;
+  }
 }
 </style>
